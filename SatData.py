84a7f42070326d1c1b0c996c41c3fb11f7a3aaac
wunzt <wunzt@oregonstate.edu>
2022-07-10 18:31:19 -0700

    Assignment 5.c

--- SatData.py
+++ SatData.py
@@ -1,23 +1,25 @@
 # Author: Thomas Wunz
 # GitHub username: wunzt
 # Date: 7/10/2022
-# Description:
+# Description: Creates a dictionary of SAT data and returns a .csv file containing a list of data based upon DBN input,
+#                sorted by DBN.
 
 import json
 
 
 class SatData:
-    """___"""
+    """Creates a dictionary of SAT data and returns a .csv file containing a list of data based upon DBN input,
+    sorted by DBN."""
 
     def __init__(self):
-        """___"""
+        """Creates a dictionary of SAT data from a passed file."""
         with open("sat.json", "r") as infile:
             sat_data_dict = json.load(infile)
 
         self._sat_data = sat_data_dict
 
     def save_as_csv(self, dbn_list):
-        """___"""
+        """Returns a .csv file containing a list of data based upon DBN input, sorted by DBN."""
 
         dbn_list.sort()
 
@@ -27,12 +29,16 @@ class SatData:
 
             for value in dbn_list:
                 for school in self._sat_data["data"]:
-                    if value == self._sat_data["data"][8]:
-                        dbn = self._sat_data["data"][8]
-                        school_name = self._sat_data["data"][9]
-                        test_takers = self._sat_data["data"][10]
-                        reading_mean = self._sat_data["data"][11]
-                        math_mean = self._sat_data["data"][12]
-                        writing_mean = self._sat_data["data"][13]
+                    if value == school[8]:
+
+                        dbn = school[8]
+                        school_name = school[9]
+                        if "," in school_name:
+                            school_name = '"' + school_name + '"'
+                        test_takers = school[10]
+                        reading_mean = school[11]
+                        math_mean = school[12]
+                        writing_mean = school[13]
+
                         outfile.write(dbn + "," + school_name + "," + test_takers + "," + reading_mean +
                                       "," + math_mean + "," + writing_mean + "\n")
